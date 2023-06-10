be459db4bc0198be4a3a3d23e61ab8fe5ab313e2
ZhengjunHUO <firelouiszj@hotmail.com>
2023-06-10 12:17:12 +0000

    fix test part
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- libbpf-rs/tests/test.rs
+++ libbpf-rs/tests/test.rs
@@ -280,52 +280,89 @@ fn test_object_map_empty_lookup() {
         .is_none());
 }
 
+/// Test CRUD operations on map of type queue and stack
 #[test]
 fn test_object_map_queue_crud() {
     bump_rlimit_mlock();
 
-    let obj = get_test_object("cgroup-fw.bpf.o");
-    let data_flow = obj.map("data_flow").expect("failed to find map");
+    let obj = get_test_object("tracepoint.bpf.o");
+    let queue = obj.map("queue").expect("failed to find queue map");
+    let stack = obj.map("stack").expect("failed to find stack map");
 
     let key: [u8; 0] = [];
     let value1: Vec<u8> = std::iter::successors(Some(0), |&n| Some(n + 1))
-        .take(16)
+        .take(4)
         .collect();
-    let value2: Vec<u8> = std::iter::successors(Some(16), |&n| Some(n + 1))
-        .take(16)
+    let value2: Vec<u8> = std::iter::successors(Some(4), |&n| Some(n + 1))
+        .take(4)
         .collect();
 
-    data_flow
+    // Test queue, FIFO expected
+    queue
         .update(&key, &value1, MapFlags::ANY)
-        .expect("failed to update");
-    data_flow
+        .expect("failed to update in queue");
+    queue
         .update(&key, &value2, MapFlags::ANY)
-        .expect("failed to update");
+        .expect("failed to update in queue");
 
-    let mut val = data_flow
+    let mut val = queue
         .lookup(&key, MapFlags::ANY)
-        .expect("failed to pop from queue")
+        .expect("failed to peek the queue")
         .expect("failed to retrieve value");
-    assert_eq!(val.len(), 16);
+    assert_eq!(val.len(), 4);
     assert_eq!(&val, &value1);
 
-    val = data_flow
+    val = queue
         .lookup_and_delete(&key)
         .expect("failed to pop from queue")
         .expect("failed to retrieve value");
-    assert_eq!(val.len(), 16);
+    assert_eq!(val.len(), 4);
     assert_eq!(&val, &value1);
 
-    val = data_flow
+    val = queue
         .lookup_and_delete(&key)
         .expect("failed to pop from queue")
         .expect("failed to retrieve value");
-    assert_eq!(val.len(), 16);
+    assert_eq!(val.len(), 4);
     assert_eq!(&val, &value2);
 
-    assert!(data_flow
+    assert!(queue
         .lookup_and_delete(&key)
         .expect("failed to pop from queue")
+        .is_none());
+
+    // Test stack, LIFO expected
+    stack
+        .update(&key, &value1, MapFlags::ANY)
+        .expect("failed to update in stack");
+    stack
+        .update(&key, &value2, MapFlags::ANY)
+        .expect("failed to update in stack");
+
+    val = stack
+        .lookup(&key, MapFlags::ANY)
+        .expect("failed to pop from stack")
+        .expect("failed to retrieve value");
+    assert_eq!(val.len(), 4);
+    assert_eq!(&val, &value2);
+
+    val = stack
+        .lookup_and_delete(&key)
+        .expect("failed to pop from stack")
+        .expect("failed to retrieve value");
+    assert_eq!(val.len(), 4);
+    assert_eq!(&val, &value2);
+
+    val = stack
+        .lookup_and_delete(&key)
+        .expect("failed to pop from stack")
+        .expect("failed to retrieve value");
+    assert_eq!(val.len(), 4);
+    assert_eq!(&val, &value1);
+
+    assert!(stack
+        .lookup_and_delete(&key)
+        .expect("failed to pop from stack")
         .is_none());
 }
 
